49003f51b541d62e956880886250fd3e0b51c663
Ithamar Francois <77698337+IthamarFrancois@users.noreply.github.com>
2021-02-24 00:27:31 -0500

    This is a commit for the messy rough draft(s) I have for working on PyBank.

--- PyBank/Main_PyBank.py
+++ PyBank/Main_PyBank.py
@@ -2,66 +2,176 @@ import os       # First we'll import the os module which will allow us to create
 import csv      # And csv module for reading CSV files
 bankpath_csv = os.path.join('Resources', 'budget_data.csv')
 
-#Testing of Write File Dest
+# Testing of Write File Dest
 # Specify the file to write to
 output_csv = os.path.join("Analysis", "Test.csv")
 
-# save the output file path
-#output_file = os.path.join("output.csv")
-
-#ALWAYS MAKE SURE TO DO THE ABOVE FOR READ/WRITE
-# import os
-# import csv
-# x (x_csv) = os.path.join("..", "Folder where file is located", "FileToBeRead.csv")
-
 # ------------------------------------------------------------------------------------------------------
 
-Months = []
-TotalMo = []
-Amounts = []
-TotalAmt = []
-ChangeAvg = []
-GreatestIncr = []
-GreatestDecr = []
-
 # Open and read csv
-with open(bankpath_csv, newline='', encoding='utf-8') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-
-    # Read the header row first (skip this part if there is no header)
-    csv_header = next(csv_file)
+with open(bankpath_csv, newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+        # Read the header row first (skip this part if there is no header)
+    csv_header = next(csv_reader)
     print(f"Header: {csv_header}")
+    
 
-        # Read through each row of data after the header
+    # Definitions of variables: --------------------------------------------------------------------------------------
+    MonthCounter = 0                 # The total number of months included in the dataset
+    TotalAmount = 0                  # The net total amount of "Profit/Losses" over the entire period
+    
+
+    ProfitThisMonth = 0              # Tracker for the profit gain/loss this particular month
+    PreviousMonthProfit = 0          # Reset start amount for profit values to calculate next "Profit/Losses"
+    
+    MonthlyChange = 0
+    ProfLossRange = 0
+    AvgProfLossChange = 0
+    
+    GreatestInc = 0
+    GreatestMonth = 0
+    GreatestDec = 0
+    LowestMonth = 0
+
+    Date_Range = []                  # Reading row[0] data
+    Profit_Loss_Amounts = []         # Reading row[1] data
+    Profit_Change = []               # To calc the average
+    Greatest_Increase = []           # The greatest increase in profits (date and amount) over the entire period  
+    Greatest_Decrease = []           # The greatest decrease in losses (date and amount) over the entire period
+
+    Date_Data = []
+    Profit_Data = []
+    
+
+
+    #raise SystemExit
+# Code start: --------------------------------------------------------------------------------------
+    i=0
+    j=0
     for row in csv_reader:
-        Months = row[0].split('-')
-
+        Date_Range.append(row[0])
+        Profit_Loss_Amounts.append(row[1])
         
         
-        #Months.append(row.split("-"))
-        print(Months)
-        #TotalAmt.append(row[1])
+        Date_Data.append(row[0])
+        Profit_Data.append(row[1])
+        #RowData = zip(Row_Data,Profit_Loss_Amounts)
+
         
-        #print(row)
+        #Profit_Change.append(row[0])
+        ProfitThisMonth = int(row[1])
+        MonthlyChange = (ProfitThisMonth - PreviousMonthProfit)
+        Profit_Change.append(MonthlyChange)
+        
+        PreviousMonthProfit = ProfitThisMonth
+
+        
+        MonthCounter += 1
+        TotalAmount = int(row[1]) + TotalAmount
+        
+    
+    j = 0
+    ProfLossRange = 0
+    for i in range(len(Profit_Change)):
+        ProfLossRange = ProfLossRange + Profit_Change[i]
+        if (Profit_Change[i] > 0) and (Profit_Change[i] > GreatestInc):
+            GreatestInc = Profit_Change[i]
+            GreatestMonth = Date_Data[i]
+        if (Profit_Change[i] < 0) and (Profit_Change[i] < GreatestDec):
+            GreatestDec = Profit_Change[i]
+            LowestMonth = Date_Data[i]
+        print(Profit_Change[i])
+
+    AvgProfLossChange = ProfLossRange / len(Profit_Change)
+
+    print('--------------------------------------' + '\n') 
+    print(i)
+    print(MonthCounter)
+    print(AvgProfLossChange)
+    print(f'Greatest Month was: ' + str(GreatestMonth) + ' and the Greatest Month total was: $' + str(GreatestInc) + '')
+    print(f'Lowest Month was: ' + str(LowestMonth) + ' and the Lowest Month total was: $' + str(GreatestDec) + '')
+        
+        
+        #if  ProfLossRange == 0:
+            #ProfLossRange = int(list(row[1]))
+            #print(ProfLossRange)
+            #print("Success")
+        #else:
+            #print("Fail")
+            #break
+        
+            #j += 1
+
+            #i = int(i + 1)
+            #if row[1] == "310503":
+                #print("Success")
+            #break
+            #print(row) 
+
+    print('--------------------------------------2' + '\n') 
+    #print(Date_Data[j]) 
+    #print(Profit_Data[i])
+    
+      
+    raise SystemExit  
+        
+        #test = row[1]
+        #if RowDataTest == Row_Data:
+            #print("Success")
+            #j = i
+        #else:
+            #print("Failed")
+
+    
+    #print('--------------------------------------2' + '\n')    
+    #print(j)
+    #print(i)
+
+    #for amt in Profit_Loss_Amounts:
+        #Total_Amount = (int(amt) + Total_Amount)
+        
+        #print(amt)
+        #print(Total_Amount)
+    
+    #for chg in Profit_Change:
+        #print(chg)
+
+#print(f'Total Months: ' + str(Total_Month_Counter) + ' Profit Amount total is: $' + str(Total_Amount) + '')
 
 
-#for i in range(len(candy_list)):
-    #print("[" + str(i) + "]" + candy_list[i])
+
+#Month_Count = (len(DateRange))    # = 86
+
+# We can also add conditional logic (if statements) to a list comprehension
+#NORMAL WAY
 
 
 
 
 
+#july_temperatures = [87, 85, 92, 79, 106]
+#hot_days = []
+#for temperature in july_temperatures:
+#    if temperature > 90:
+#        hot_days.append(temperature)
+
+#print(hot_days)
+#print(hot_days[0])
+#print(hot_days[1])
+
+#print('---------------------------------------4' + '\n')
 
 
+# List Comprehension of above with If conditional built-in
+#hot_days = [temperature for temperature in july_temperatures if temperature > 90]
+#print(hot_days)
 
 
+    
+   
+    #raise SystemExit  
 
-
-
-
-
-    #Formatting End Summary Goal
+          #Formatting End Summary Goal
 #print(f"Financial Analysis")
 #print(f"----------------------------")
 #print(f"Total Months: XXX")
@@ -70,26 +180,11 @@ with open(bankpath_csv, newline='', encoding='utf-8') as csv_file:
 #print(f"Greatest Increase in Profits: XXX $XXX")
 #print(f"Greatest Decrease in Profits: XXX $XXX")
 
-    
-    #Testing of Write File Dest
-# Specify the file to write to
-#output_csv = os.path.join("Analysis", "Test.csv")
-#output_csv.write("Testing - Ithamar Francois")
 
 
 # Open the file using "write" mode. Specify the variable to hold the contents
-with open(output_csv, 'w', newline='') as csvfile:
-    # Initialize csv.writer
-    csv_writer = csv.writer(csvfile, delimiter=',')
-    csv_writer.writerow(["Testing", "Ithamar", "Francois"])
-    
-    
-    
-    # Initialize csv.writer
-#    csvwriter = csv.writer(csvfile, delimiter=',')
-
-    # Write the first row (column headers)
-#   csvwriter.writerow(['First Name', 'Last Name', 'SSN'])
-
-    # Write the second row
-#    csvwriter.writerow(['Caleb', 'Frost', '505-80-2901'])
+#with open(output_csv, 'w', newline='') as csvfile:
+    #   Initialize csv.writer
+    #   csv_writer = csv.writer(csvfile, delimiter=',')
+    #   csv_writer.writerow(["Testing", "Ithamar", "Francois"])
+    #   csv_writer.writerows([])
--- PyPoll/Main_PyPoll.py
+++ PyPoll/Main_PyPoll.py
@@ -1,2 +1,233 @@
 import os       # First we'll import the os module which will allow us to create file paths across operating systems
 import csv      # And csv module for reading CSV files
+
+
+# Using this py for scrap paper while working on code for Main_PyBank
+
+
+
+
+# ------------------------------------------------------------------------------------------------------
+
+Total_Month_Counter = 0         # The total number of months included in the dataset
+Total_Amount = 0                # The net total amount of "Profit/Losses" over the entire period
+Profit_Loss = 0                 # To calculate the changes in "Profit/Losses"
+Average_Of_Changes = 0          # Average of Profit_Lose changes
+#TotalMonths = len(row)
+
+Greatest_Increase = []          # The greatest increase in profits (date and amount) over the entire period  
+Greatest_Decrease = []          # The greatest decrease in losses (date and amount) over the entire period
+
+#Define rows (after Split) to hold new lists
+Changing_Months = []
+Changing_Years = []
+Amounts_Values = []
+
+
+# Open and read csv
+with open(bankpath_csv, newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+
+    # Read the header row first (skip this part if there is no header)
+    csv_header = next(csvfile)
+    print(f"Header: {csv_header}")
+    Total_Month_Counter = Total_Month_Counter + 1
+    
+    
+    #row[1] = next(csvfile)
+    #Total_Month_Counter = Total_Month_Counter + 1
+    #print(row)
+    
+        # Read through each row of data after the header
+    for row in csv_reader:
+        Changing_Months.append((row[0].split('-'))
+            #Changing_Years.append(row[1])
+            #Amounts_Values.append(row[2])
+        
+            #Total_Amount.append(row[0])
+    #   Total_Month_Counter = Total_Month_Counter + 1
+        
+        
+            #Changing_Months.append(row.split("-"))
+            #print((Changing_Months), (Total_Amount)) 
+        
+            print(Changing_Months[0])
+            #Total_Amt.append(row[1])
+        
+        #print(row)
+
+
+#for i in range(len(candy_list)):
+    #print("[" + str(i) + "]" + candy_list[i])
+
+
+
+
+
+
+
+
+
+---------------------------------------------------------------------------
+
+# Read each row of data after the header
+    for row in csvreader:
+        print(row)
+
+
+        Total_Month_Counter = 0
+
+
+        # Read through each row of data after the header
+    for row in csv_reader:
+        #Changing_Months = row[0].split('-')
+        Changing_Months.append(row[0])
+        Changing_Years.append(row[1])
+        #Amounts_Values.append(row[2])
+        
+        #Total_Amount.append(row[0])
+        Total_Month_Counter = Total_Month_Counter + 1
+        
+        
+        #Changing_Months.append(row.split("-"))
+        #print((Changing_Months), (Total_Amount)) 
+        
+        print(Changing_Months)
+        #print(Changing_Years[1])
+        #Total_Amt.append(row[1])
+        
+        #print(row)
+
+
+#for i in range(len(candy_list)):
+    #print("[" + str(i) + "]" + candy_list[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #Formatting End Summary Goal
+#print(f"Financial Analysis")
+#print(f"----------------------------")
+#print(f"Total Months: XXX")
+#print(f"Total: $XXX"
+#print(f"Average Change: $XXX")
+#print(f"Greatest Increase in Profits: XXX $XXX")
+#print(f"Greatest Decrease in Profits: XXX $XXX")
+
+    
+    #Testing of Write File Dest
+# Specify the file to write to
+#output_csv = os.path.join("Analysis", "Test.csv")
+#output_csv.write("Testing - Ithamar Francois")
+
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+#with open(output_csv, 'w', newline='') as csvfile:
+    # Initialize csv.writer
+    #csv_writer = csv.writer(csvfile, delimiter=',')
+   # csv_writer.writerow(["Testing", "Ithamar", "Francois"])
+    #csv_writer.writerows([])
+    
+    
+    
+    # Initialize csv.writer
+#    csvwriter = csv.writer(csvfile, delimiter=',')
+
+    # Write the first row (column headers)
+#   csvwriter.writerow(['First Name', 'Last Name', 'SSN'])
+
+    # Write the second row
+#    csvwriter.writerow(['Caleb', 'Frost', '505-80-2901'])
+
+
+
+
+
+
+#----------------------------------------------------------------------
+      #Define rows to hold new lists
+Changing_Months = []
+Changing_Years = []
+Amounts_Values = []
+
+      
+      
+        All_Row_Data = row[0].split('-')
+        All_Row_Data.append(row[1])
+        
+        ##Changing_Months = All_Row_Data[0]
+        ##Changing_Years = All_Row_Data[1]
+        ##Amounts_Values = All_Row_Data[2]
+        
+        Changing_Months = All_Row_Data[0]
+        Changing_Years = All_Row_Data[1]
+        Amounts_Values = All_Row_Data[2]
+        print(f" {Changing_Months} , {Changing_Years}, {Amounts_Values}")
+        
+        Total_Month_Counter += 1
+        Total_Amount = int(Total_Amount + int(Amounts_Values))
+    
+print(f'Total Months Counted: ' + str(Total_Month_Counter) + ' and the Profit Amount total is: $' + str(Total_Amount) + '')
+    
+        
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+#with open(output_csv, 'w', newline='') as csvfile:
+    # Initialize csv.writer
+    #csvwriter = csv.writer(csvfile, delimiter=',')
+    #csvwriter.writerow(['Month', 'Year', 'Profit/Loss'])
+
+    #csvwriter.writerows([Changing_Months, Changing_Years, Amounts_Values])
+
+        #----------------------------------------------------------------------
+
+
+
+
+
+
+
+
+        #Changing_Months = row[0].split('-')
+        #Changing_Months.append(row[0])
+        #All_Row_Data = row[0].split('-')
+        #All_Row_Data.append(row[1])
+
+        #Changing_Years = row[0].split('-')
+        #Changing_Years.append(row[1])
+        
+        #Amounts_Values = Changing_Years[2]
+
+        #Changing_Months = Changing_Years[0]
+        #Amounts_Values = Changing_Years[2]
+
+
+        #Amounts_Values = row[0].split('-')
+        #Amounts_Values.append(row[1])
+
+        #print(All_Row_Data)
+        
+        #print(Changing_Months[0], Changing_Years[1], Amounts_Values[2])
+        #print(f" {Changing_Months} , {Changing_Years}, {Amounts_Values}")
+        #print((Changing_Months)
+        #print(Changing_Years)
+        #print(Amounts_Values)
+
+        #AllData = zip(Changing_Months, Changing_Years, Amounts_Values)
+        #print(list(AllData)[0])
+
+
+
+
+        
+        #print(Changing_Months)
